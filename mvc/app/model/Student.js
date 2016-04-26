f676d583abb541ddd9890d398e54e2e84e4c48bf
zhangzhongwang <727271755@qq.com>
2016-04-26 17:31:16 +0800

    model

---
+++ mvc/app/model/Student.js
@@ -0,0 +1,4 @@
+Ext.define('RUN.model.Student', {
+    extend: 'Ext.data.Model',
+    fields: ['id','name','age','sex']
+});
--- mvc/app/store/Students.js
+++ mvc/app/store/Students.js
@@ -1,6 +1,7 @@
 Ext.define('RUN.store.Students', {
     extend: 'Ext.data.Store',
-    fields: ['id','name', 'age','sex'],
+    //fields: ['id','name', 'age','sex'],
+    model: 'RUN.model.Student',
     data: [
         {id:1,name: '张三1',    age: 30,sex:'男'},
         {id:2,name: '李四',    age: 20,sex:'女'}
