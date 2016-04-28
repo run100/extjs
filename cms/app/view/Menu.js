d1fe0e14d43cd2713fc5c174019c50b61292dd3f
zhangzhongwang <727271755@qq.com>
2016-04-28 11:00:03 +0800

    menu.js

--- cms/app/view/Menu.js
+++ cms/app/view/Menu.js
@@ -7,8 +7,36 @@ Ext.define('cms.view.Menu', {
       };
       this.items = [
         {
-            text: '区块',
-            href: '/news/admin/block'
+          text: '区块',
+          href: 'http://baidu.com'
+        },
+        {
+          text: '一级菜单',
+          menu: {
+            xtype: 'menu',
+            items: [
+              {
+                text: '二级菜单',
+                href: 'http://blog.chromev.com'
+              },
+              {
+                text: '来个测试',
+                href: 'http://blog.chromev.com'
+              },
+              {
+                text: '系统管理',
+                menu: {
+                  xtype: 'menu',
+                  items: [
+                    {
+                      text: '角色管理',
+                      href: 'http://blog.chromev.com'
+                    }
+                  ]
+                }
+              },
+            ]
+          }
         }
       ];
       this.callParent();
